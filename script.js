83b3957bb2e4309cdc74cc3d8aa9d62d5ea37e33
Rkunski <87666423+Rkunski@users.noreply.github.com>
2021-08-29 22:02:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ var pirmas = array[0];
 var last = array[array.length - 1];
 console.log("pirmas elementas: " + pirmas + " paskutinis elementas: " + last);
 for (var i = 0; i < skaicius; i++) {
-    console.log(array[i]);
+    console.log(array[skaicius-1]);
   }
